37b4146761765629457ffc84ab051e3926142751
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-03-08 08:10:06 +0900

    Add:Routing Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
-import { useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
+
+const About = () => <div data-testid="about">You are on the about page</div>;
+const Home = () => <div data-testid="home">You are home</div>;
+const NoMatch = () => <div>No match</div>;
+
+export const LocationDisplay = () => {
+  const location = useLocation();
+
+  return <div data-testid="location-display">{location.pathname}</div>;
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </div>
   );
 }
--- src/__tests__/integration/App.test.tsx
+++ src/__tests__/integration/App.test.tsx
@@ -1,12 +1,48 @@
 import { render, screen } from "@testing-library/react";
-import React from "react";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import { describe, test } from "vitest";
-import App from "../../App";
+import App, { LocationDisplay } from "../../App";
 
 describe("App", () => {
-  test("renders App component", () => {
-    render(<App />);
+  test("full app rendering/navigating", async () => {
+    render(<App />, { wrapper: BrowserRouter });
+    const user = userEvent.setup();
 
-    screen.debug();
+    // verify page content for default route
+    expect(screen.getByText(/you are home/i));
+    expect(screen.getByTestId("home"));
+
+    // verify page content for expected route after navigating
+    await user.click(screen.getByText(/about/i));
+    expect(screen.getByText(/you are on the about page/i));
+  });
+
+  test("landing on a bad page", () => {
+    const badRoute = "/some/bad/route";
+
+    // use <MemoryRouter> when you want to manually control the history
+    render(
+      <MemoryRouter initialEntries={[badRoute]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    // verify navigation to "no match" route
+    expect(screen.getByText(/no match/i));
+  });
+
+  test("rendering a component that uses useLocation", () => {
+    const route = "/some-route";
+
+    // use <MemoryRouter> when you want to manually control the history
+    render(
+      <MemoryRouter initialEntries={[route]}>
+        <LocationDisplay />
+      </MemoryRouter>
+    );
+
+    // verify location display is rendered
+    expect(screen.getByTestId("location-display"));
   });
 });
