62d343acca5d00dfc7514afd4c66d3c6acae0de4
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-09-28 08:28:08 +0900

    Add:localStorage sample

--- src/pages/LocalStorage.tsx
+++ src/pages/LocalStorage.tsx
@@ -1,5 +1,33 @@
 import { FC } from "react";
+import { useForm } from "react-hook-form";
+
+const BOOK_IN_RENTAL_KEY = "book-in-rental";
+
+export const getBookInRental = () => localStorage.getItem(BOOK_IN_RENTAL_KEY);
+
+export const setBookInRental = (book: string) =>
+  localStorage.setItem(BOOK_IN_RENTAL_KEY, book);
 
 export const LocalStorage: FC = () => {
-  return <>samples of localStorage</>;
+  const { register, handleSubmit } = useForm({
+    defaultValues: { newBook: "" },
+  });
+  const bookInRental = getBookInRental();
+
+  return (
+    <>
+      <h2>Book In Rental</h2>
+      <div>Book in rental: {bookInRental || "none"}</div>
+      <form
+        onSubmit={handleSubmit((data) => {
+          setBookInRental(data.newBook);
+          location.reload();
+        })}
+      >
+        <label htmlFor="newBook">New Book: </label>
+        <input type="text" id="newBook" {...register("newBook")} />
+        <input type="submit" />
+      </form>
+    </>
+  );
 };
