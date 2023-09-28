50f5129551be8ad6710c87f4bb9837771a5f4eae
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-09-28 09:38:58 +0900

    Improve:use setBookInRental

--- src/__tests__/unit/localStorage.test.ts
+++ src/__tests__/unit/localStorage.test.ts
@@ -1,10 +1,10 @@
 import { describe, expect, test } from "vitest";
-import { BOOK_IN_RENTAL_KEY, getBookInRental } from "../../pages/LocalStorage";
+import { getBookInRental, setBookInRental } from "../../pages/LocalStorage";
 
-describe("localStorage mock", () => {
+describe("localStorage tests", () => {
   test("get a book in rental from localStorage", () => {
     const DUMMY_BOOK_IN_RENTAL = "test book";
-    localStorage.setItem(BOOK_IN_RENTAL_KEY, DUMMY_BOOK_IN_RENTAL);
+    setBookInRental(DUMMY_BOOK_IN_RENTAL);
 
     expect(getBookInRental()).toBe(DUMMY_BOOK_IN_RENTAL);
   });
