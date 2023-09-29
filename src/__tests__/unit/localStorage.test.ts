7609af39decee05be969d78ca0e9fd6a435eec9d
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-09-29 09:40:30 +0900

    Add:clear localStorage after each test

--- src/__tests__/unit/localStorage.test.ts
+++ src/__tests__/unit/localStorage.test.ts
@@ -2,14 +2,13 @@ import { describe, expect, test } from "vitest";
 import { getBookInRental, setBookInRental } from "../../pages/LocalStorage";
 
 describe("localStorage tests", () => {
+  afterEach(() => {
+    localStorage.clear();
+  });
   test("get a book in rental from localStorage", () => {
     const DUMMY_BOOK_IN_RENTAL = "test book";
     setBookInRental(DUMMY_BOOK_IN_RENTAL);
 
-    expect(getBookInRental()).toBe(DUMMY_BOOK_IN_RENTAL);
-  });
-  test("get a book in rental from localStorage without setting it", () => {
-    const DUMMY_BOOK_IN_RENTAL = "test book";
     expect(getBookInRental()).toBe(DUMMY_BOOK_IN_RENTAL);
   });
 });
