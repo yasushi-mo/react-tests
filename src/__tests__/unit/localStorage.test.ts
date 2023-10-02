82901c4dd36ff3f0532639a8727d155d24f17934
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-10-02 09:30:13 +0900

    Add:spyOn getItem

--- src/__tests__/unit/localStorage.test.ts
+++ src/__tests__/unit/localStorage.test.ts
@@ -1,8 +1,15 @@
 import { describe, expect, test } from "vitest";
-import { getBookInRental, setBookInRental } from "../../pages/LocalStorage";
+import {
+  BOOK_IN_RENTAL_KEY,
+  getBookInRental,
+  setBookInRental,
+} from "../../pages/LocalStorage";
 
 describe("localStorage tests", () => {
+  const getBookInRentalSpy = vi.spyOn(Storage.prototype, "getItem");
+
   afterEach(() => {
+    getBookInRentalSpy.mockClear();
     localStorage.clear();
   });
   test("get a book in rental from localStorage", () => {
@@ -10,6 +17,7 @@ describe("localStorage tests", () => {
     setBookInRental(DUMMY_BOOK_IN_RENTAL);
 
     expect(getBookInRental()).toBe(DUMMY_BOOK_IN_RENTAL);
+    expect(getBookInRentalSpy).toHaveBeenCalledWith(BOOK_IN_RENTAL_KEY);
   });
   test("not get a book in rental from localStorage", () => {
     const DUMMY_BOOK_IN_RENTAL = "test book";
