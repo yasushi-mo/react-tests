f23617732d1ca1c928718f818c260cd2ededf2df
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-10-04 09:09:50 +0900

    Add:setItemSpy toHaveBeenCalledWith

--- src/__tests__/unit/localStorage.test.ts
+++ src/__tests__/unit/localStorage.test.ts
@@ -7,14 +7,21 @@ import {
 
 describe("localStorage tests", () => {
   const getItemSpy = vi.spyOn(Storage.prototype, "getItem");
+  const setItemSpy = vi.spyOn(Storage.prototype, "setItem");
 
   afterEach(() => {
     getItemSpy.mockClear();
+    setItemSpy.mockClear();
     localStorage.clear();
   });
-  test("get a book in rental from localStorage", () => {
+  test("set and get a book in rental from localStorage", () => {
     const DUMMY_BOOK_IN_RENTAL = "test book";
+
     setBookInRental(DUMMY_BOOK_IN_RENTAL);
+    expect(setItemSpy).toHaveBeenCalledWith(
+      BOOK_IN_RENTAL_KEY,
+      DUMMY_BOOK_IN_RENTAL
+    );
 
     expect(getBookInRental()).toBe(DUMMY_BOOK_IN_RENTAL);
     expect(getItemSpy).toHaveBeenCalledWith(BOOK_IN_RENTAL_KEY);
