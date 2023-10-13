bf34a8173654e8267d99ece279a7c28a87bbad6e
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-10-13 09:06:58 +0900

    Add: PostCSS plugins and postcss-preset-mantine

---
+++ src/libs/mantine/index.ts
@@ -0,0 +1,3 @@
+import { createTheme, MantineThemeOverride } from "@mantine/core";
+
+export const theme: MantineThemeOverride = createTheme({});
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
+import { MantineProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { theme } from "./libs/mantine";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <MantineProvider theme={theme}>
+        <App />
+      </MantineProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
