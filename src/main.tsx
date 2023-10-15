958ed15b2f2b325ad6ec922bb5086a30a32dc53c
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-10-16 08:44:04 +0900

    Remove:Mantine provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
-import { MantineProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { theme } from "./libs/mantine";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <MantineProvider theme={theme}>
-        <App />
-      </MantineProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
