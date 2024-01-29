a5b8def36de920cea3a14d8cb57d29416bd9c0e4
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-01-30 05:59:13 +0900

    Add:generate key function

--- src/pages/Crypto.tsx
+++ src/pages/Crypto.tsx
@@ -1,9 +1,15 @@
 import { FC } from "react";
+import { generateCryptoKey } from "../libs/crypto";
 
 export const Crypto: FC = () => {
+  const onGenerateKey = async () => {
+    console.log(await generateCryptoKey());
+  };
+
   return (
     <div>
       <h2>Crypto</h2>
+      <button onClick={onGenerateKey}>Generate Key</button>
     </div>
   );
 };
