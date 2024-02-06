fc12f62417026fdd696c6dc02ee4551cf74a0fba
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-02-06 21:35:25 +0900

    Add:URI encoding

---
+++ src/libs/encode/uriEncoding.ts
@@ -0,0 +1,16 @@
+export const reservedCharacters = ";,/?:@&=+$#";
+export const unescapedCharacters = "-_.!~*'()";
+export const alphanumericCharacters = "ABCabc123";
+export const nonAlphabeticCharactersAndSpace = "あ ア 亜 التثكيل ш";
+
+// console.log(encodeURI(reservedCharacters));
+// console.log(encodeURIComponent(reservedCharacters));
+
+// console.log(encodeURI(unescapedCharacters));
+// console.log(encodeURIComponent(unescapedCharacters));
+
+// console.log(encodeURI(alphanumericCharacters));
+// console.log(encodeURIComponent(alphanumericCharacters));
+
+// console.log(encodeURI(nonAlphabeticCharactersAndSpace));
+// console.log(encodeURIComponent(nonAlphabeticCharactersAndSpace));
