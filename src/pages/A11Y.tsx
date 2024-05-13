1f9f8ab43e54a4c5f8746ae0113072ea5b8a688e
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-05-13 20:50:54 +0900

    Add: anchor-is-valid

--- src/pages/A11Y.tsx
+++ src/pages/A11Y.tsx
@@ -1,6 +1,7 @@
 export const A11Y = () => {
   return (
     <div>
+      <a href="https://example.com">Example</a>
       <h3>anchor-has-content</h3>
       <p>
         <a href="https://example.com">Example</a>
