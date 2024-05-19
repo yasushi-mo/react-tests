a8ee2d88f13b5f58947dbc18bec5fa7c99f8bf8f
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-05-19 10:59:20 +0900

    Add: ChooseImage

--- src/pages/ImageUpload.tsx
+++ src/pages/ImageUpload.tsx
@@ -1,9 +1,23 @@
-import { FC } from "react";
+import { ChangeEvent, FC, useState } from "react";
 
 export const ImageUpload: FC = () => {
+  const [image, setImage] = useState("");
+
+  const handleChooseImage = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files || event.target.files.length === 0)
+      return alert("Failed to choose image");
+
+    const file = event.target.files[0];
+    setImage(URL.createObjectURL(file));
+  };
+
   return (
     <div>
       <h2>Image Upload</h2>
+      <input type="file" onChange={handleChooseImage} />
+      <button disabled={!image}>Image Upload</button>
+      <br />
+      {image ? <img src={image} alt="chosen" /> : <p>No Image Chosen</p>}
     </div>
   );
 };
