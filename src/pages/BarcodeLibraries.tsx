239cea3213863c1ed7f4d33c2ac6d8e0c9fbaa2e
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-06-25 20:48:28 +0900

    Add: Barcode

--- src/pages/BarcodeLibraries.tsx
+++ src/pages/BarcodeLibraries.tsx
@@ -1,5 +1,18 @@
+import { useBarcode } from "next-barcode";
 import { FC } from "react";
+import Barcode from "react-barcode";
 
 export const BarcodeLibraries: FC = () => {
-  return <></>;
+  const { inputRef } = useBarcode({
+    value: "12345678",
+  });
+
+  return (
+    <>
+      <h2>React Barcode</h2>
+      <Barcode value="12345678" />
+      <h2>Next Barcode</h2>
+      <svg ref={inputRef} />
+    </>
+  );
 };
