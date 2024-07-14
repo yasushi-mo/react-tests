04847319136d4f81a9f2c8dc7c5d3d4b86afc08f
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-07-14 20:00:04 +0900

    Update: Generics Type

--- src/libs/types/genericsType.ts
+++ src/libs/types/genericsType.ts
@@ -1,6 +1,8 @@
-export function identity<T>(arg: T): T {
+export function identity<T extends string | number>(arg: T): T {
   return arg;
 }
 
 identity<number>(1);
 identity<string>("test");
+
+// identity<boolean>(true);
