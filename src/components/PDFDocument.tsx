aa6f8c951bf1204a480a2d47c7cb616a8fee7cfe
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-08-09 09:01:20 +0900

    Update: Component structure

---
+++ src/components/PDFDocument.tsx
@@ -0,0 +1,27 @@
+import { Document, Page, StyleSheet, Text, View } from "@react-pdf/renderer";
+import { FC } from "react";
+
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#E4E4E4",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+});
+
+export const PDFDocument: FC = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
--- src/pages/PDF.tsx
+++ src/pages/PDF.tsx
@@ -1,37 +1,6 @@
-import {
-  Document,
-  Page,
-  PDFViewer,
-  StyleSheet,
-  Text,
-  View,
-} from "@react-pdf/renderer";
+import { PDFViewer } from "@react-pdf/renderer";
 import { FC } from "react";
-
-const styles = StyleSheet.create({
-  page: {
-    flexDirection: "row",
-    backgroundColor: "#E4E4E4",
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
-  },
-});
-
-const PDFDocument: FC = () => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
-    </Page>
-  </Document>
-);
+import { PDFDocument } from "../components/PDFDocument";
 
 export const PDF: FC = () => (
   <PDFViewer width="100%" height="100%">
