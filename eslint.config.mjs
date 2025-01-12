9aec457eb9565fe2a7ce9633e4fa0417ebe05ea2
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2025-01-12 11:54:32 +0900

    Update: ESLint config file

---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import react from "eslint-plugin-react";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...fixupConfigRules(compat.extends(
+    "eslint:recommended",
+    "plugin:jsx-a11y/strict",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+)), {
+    plugins: {
+        react: fixupPluginRules(react),
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "script",
+    },
+
+    rules: {
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off",
+
+        "jsx-a11y/alt-text": [2, {
+            elements: ["img", "object", "area", "input[type=\"image\"]"],
+            img: ["Image"],
+            object: ["Object"],
+            area: ["Area"],
+            "input[type=\"image\"]": ["InputImage"],
+        }],
+    },
+}];
