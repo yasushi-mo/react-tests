66f773f8f14283a1d3bdfdaf264f796477d33cb1
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2025-02-06 09:18:57 +0900

    Add: formattedIndex

--- src/libs/String/padStart.ts
+++ src/libs/String/padStart.ts
@@ -1,3 +1,9 @@
 const str = "Pizza";
 const formattedStr = str.padStart(10, ".");
 console.log(formattedStr);
+// Output: ".....Pizza"
+
+const index = 1;
+const formattedIndex = String(index).padStart(2, "0");
+console.log(formattedIndex);
+// Output: "01"
