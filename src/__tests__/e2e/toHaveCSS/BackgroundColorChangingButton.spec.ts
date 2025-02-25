1627b812169536a81a066675599467e6947d590c
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2025-02-25 09:06:26 +0900

    Add: get coverage

--- src/__tests__/e2e/toHaveCSS/BackgroundColorChangingButton.spec.ts
+++ src/__tests__/e2e/toHaveCSS/BackgroundColorChangingButton.spec.ts
@@ -1,6 +1,10 @@
 import { test, expect } from "@playwright/test";
+import fs from "fs";
 
 test("button should have correct background color", async ({ page }) => {
+  await page.coverage.startJSCoverage();
+  await page.coverage.startCSSCoverage();
+
   await page.goto("/playwright/to-have-css/background-color-changing-button");
 
   const button = page.getByRole("button", { name: "Change Background Color" });
@@ -11,4 +15,15 @@ test("button should have correct background color", async ({ page }) => {
 
   await button.click();
   await expect(button).toHaveCSS("background-color", "rgb(255, 0, 0)");
+
+  const jsCoverage = await page.coverage.stopJSCoverage();
+  fs.writeFileSync(
+    "src/__tests__/e2e/coverage-js.json",
+    JSON.stringify(jsCoverage, null, 2)
+  );
+  const cssCoverage = await page.coverage.stopCSSCoverage();
+  fs.writeFileSync(
+    "src/__tests__/e2e/coverage-css.json",
+    JSON.stringify(cssCoverage, null, 2)
+  );
 });
