0490393d8b668dcf8e6a2a10c24b5b6d70c53e34
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2025-03-12 09:21:39 +0900

    Add: toReversed

--- src/libs/array/reverse.ts
+++ src/libs/array/reverse.ts
@@ -10,3 +10,16 @@ export const reverse = () => {
   console.log(array);
   // Output: Array [3, 2, 1]
 };
+
+export const toReversed = () => {
+  const array = [1, 2, 3];
+  console.log(array);
+  // Output: Array [1, 2, 3]
+
+  const reversed = array.toReversed();
+  console.log(reversed);
+  // Output: Array [3, 2, 1]
+
+  console.log(array);
+  // Output: Array [1, 2, 3]
+};
